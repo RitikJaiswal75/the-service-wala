ebb8eb940a6e7fa272011c02b2517783c1ef97fe
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-03-09 14:20:59 +0530

    add services card

--- src/customComponents/Card/Card.js
+++ src/customComponents/Card/Card.js
@@ -1,10 +1,19 @@
 import "./Card.css";
 
 function Card(props) {
+  const items = [];
+  props.items.forEach((element) => {
+    items.push(<li key={element}>✅ {element}</li>);
+  });
   return (
     <div className="card">
-      <div className="image-container"></div>
-      <div className="description"></div>
+      <div className="image-container">
+        <img src={props.image} alt={props.image}></img>
+      </div>
+      <div className="description">
+        <h3 className="card-heading">{props.heading}</h3>
+        <ul>{items}</ul>
+      </div>
     </div>
   );
 }
--- src/customComponents/Section/Section.js
+++ src/customComponents/Section/Section.js
@@ -1,15 +1,22 @@
 import Card from "../Card/Card";
 import "./Section.css";
 function Section(props) {
+  const cards = [];
+  for (let i = 0; i < props.images.length; i++) {
+    cards.push(
+      <Card
+        key={i}
+        image={props.images[i]}
+        heading={props.headings[i]}
+        items={props.items[i]}
+      ></Card>
+    );
+  }
   return (
     <section className="content" id={props.name}>
       <h1>{props.name}</h1>
       <h2>{props.short_description}</h2>
-      <div className="card-holder">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-      </div>
+      <div className="card-holder">{cards}</div>
     </section>
   );
 }
